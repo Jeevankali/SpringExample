350176f6248becc7b06a4d077e715bf08eeb87b0
Jeevan <kjeevanklp@gmail.com>
2019-01-31 00:22:15 +0530

    First commit

---
+++ src/main/java/com/mytectra/springExample/SpringExampleApplication.java
@@ -0,0 +1,26 @@
+package com.mytectra.springExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	@GetMapping(path="/first")
+	@ResponseBody
+	public String sampleMethod(){
+		
+		return "it's working buddy";
+		
+	}
+}
+
